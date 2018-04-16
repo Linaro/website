3f4fce2c2fbbdb673a12b62b9f959b0d0b9e1f01
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-04-16 14:58:55 +0100

    Updated the name of a sass partial

--- assets/js/package-about.js
+++ assets/js/package-about.js
@@ -5,7 +5,7 @@
 //= require vendor/lazysizes
 //= require vendor/ls.unveilhooks
 //= require vendor/lightbox
-//= require app/sticky-navbar
+//= require app/sticky-tab-bar
 
 //= require app/main
 //= require app/about
