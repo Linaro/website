8b82f054327efde3a6fe6a1dfeae239073384310
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-05-14 17:10:20 +0100

    Removed JS incluides that were no longer needed

--- assets/js/package-about.js
+++ assets/js/package-about.js
@@ -5,7 +5,6 @@
 //= require vendor/owl.carousel
 //= require vendor/lazysizes
 //= require vendor/ls.unveilhooks
-//= require vendor/lightbox
 //= require app/sticky-tab-bar
 
 //= require app/main
--- assets/js/package-home.js
+++ assets/js/package-home.js
@@ -4,7 +4,6 @@
 //= require vendor/cookieconsent
 //= require vendor/lazysizes
 //= require vendor/owl.carousel
-//= require vendor/twitter
 
 //= require app/main
 //= require app/home
