89e9b9c7a23c681f223eb46e0276881701724386
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-06-06 10:48:18 +0100

    Testing the sticky nav functionality before adding to the main JS file in the theme

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,46 +1,30 @@
 $(document).ready(function(){
-
-    // Home Stick Nav
-
-    var nav_light = $('nav.nav-light');
-    var nav_dark = $('nav.nav-dark');
-    var nav_base = $('nav#main-navigation');
-    var nav_height = nav_base.height();
-    var search_bar = $("div#search_bar");
-
-    var main_content = $('div#jumbotron-caption');
-
-    if(nav_light.length || nav_dark.length){
-
-        var origOffsetY = main_content.offset().top - nav_height;
-        
-        function scroll() {
-        
-            if ( $(window).scrollTop() > origOffsetY) {
-                if(nav_light.length){
-                    $('nav#main-navigation').removeClass('nav-light');
-                }
-                else{
-                    $('nav#main-navigation').removeClass('nav-dark');
-                }
-            }
-            else {                
-                if(nav_light.length ){
-                    $('nav#main-navigation').addClass('nav-light');
-                }
-                else{
-                    $('nav#main-navigation').addClass('nav-dark');
-                }
-            }
-        }
-        scroll();
-        
-        $(window).scroll(function(){
-                scroll();
-            });
-            
-        $(window).resize(scroll);
-    }
     
+    var stickyOffset = $('#main-navigation').offset().top;
+    var universalNav = $('#universal-nav');
+    var wrapper = $('#wrapper');
+
+    $(window).scroll(function(){
+        
+        var sticky = $('#main-navigation'),
+        scroll = $(window).scrollTop();
+
+        if (scroll >= stickyOffset) 
+        {
+          sticky.removeClass('navbar-static');
+          wrapper.css('margin-top', $('#main-navigation').height() + universalNav.height();
+          universalNav.hide();
+          sticky.addClass('navbar-fixed-top');
+        }   
+        else 
+        {
+            sticky.removeClass('navbar-fixed-top');
+            wrapper.css('margin-top', '0px');
+            universalNav.slideDown("fast");
+            sticky.addClass('navbar-static');
+        }
+        
+    });
+
     
 });
