fe2f4d51cba0d38ced96c7f926fa8a38cd85c072
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-07-20 10:29:56 +0100

    Added JQuery to collapse/expand all collapses

--- assets/js/app/members.js
+++ assets/js/app/members.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
     $('#accordion').collapse();
+    
     $(".panel-heading").click(function(){ 
-		$('#accordion .panel-heading').not(this).removeClass('isOpen');
-		$(this).toggleClass('isOpen');
-		$(this).next(".panel-collapse").addClass('thePanel');
-		$('#accordion .panel-collapse').not('.thePanel').slideUp("fast"); 
-    	$(".thePanel").slideToggle("fast").removeClass('thePanel'); 
+		// $('#accordion .panel-heading').not(this).removeClass('isOpen');
+		// $(this).toggleClass('isOpen');
+		$(this).next(".panel-collapse:not(.in)").collapse("show");
+        $(this).next(".panel-collapse.in").collapse("hide"); 
 	});
+    
+    // Expand and collapse all Collapse panels
+    $("#expand-all").click(function(){
+        if($(this).html() == "Expand All"){
+            $(".panel-collapse").collapse("show");
+            $(this).html("Collapse All");
+        }
+        else{
+            $(".panel-collapse").collapse("hide");
+            $(this).html("Expand All");
+        }
+    });
+    
+    
 });
