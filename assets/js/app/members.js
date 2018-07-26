b864e1f61f753d92dd24531e470f24989f323465
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-07-26 10:03:25 +0100

    Updated the members.js file to allow the ?expanded parameter to be passed in the url to show the page with all collapses expanded by default

--- assets/js/app/members.js
+++ assets/js/app/members.js
@@ -20,5 +20,34 @@ $(document).ready(function(){
         }
     });
     
+    function parse_query_string(query) {
+      var vars = query.split("&");
+      var query_string = {};
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        var key = decodeURIComponent(pair[0]);
+        var value = decodeURIComponent(pair[1]);
+        // If first entry with this name
+        if (typeof query_string[key] === "undefined") {
+          query_string[key] = decodeURIComponent(value);
+          // If second entry with this name
+        } else if (typeof query_string[key] === "string") {
+          var arr = [query_string[key], decodeURIComponent(value)];
+          query_string[key] = arr;
+          // If third or later entry with this name
+        } else {
+          query_string[key].push(decodeURIComponent(value));
+        }
+      }
+      return query_string;
+    }
+
+    var urlQuery = window.location.search.substring(1);
+    var queryString = parse_query_string(urlQuery);
+    
+    if(queryString.expanded){
+        $(".panel-collapse").collapse("show");
+        $("#expand-all").html("Collpase All");
+    }
     
 });
--- membership/README.md
+++ membership/README.md
@@ -15,3 +15,14 @@ The current members are listed below. If you are interested in joining these ind
 <button class="btn-primary" id="expand-all">Expand All</button>
 
 {% include members.html data-file=site.data.members %}
+
+<div class="cognito">
+    <script src="https://services.cognitoforms.com/s/KvRQmIn2dku6k6gGP711jw"></script>
+    <script>
+        Cognito.load("forms", { id: "14", entry: {
+          "PageUrl": "{{site.url}}{{page.url}}" ,
+          "RedirectUrl" : "{{site.url}}/thank-you/?ref={{page.url}}",
+          "ChoiceField": [{% for member in site.data.members %}"{{member.membership_group_name}}"{% unless forloop.last %},{% endunless %}{% endfor %}]
+        }});
+    </script>
+</div>
