9c845ff77010ec4f21f4da05063e0d25cb485f48
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-10-05 12:11:33 +0100

    Updates to the javascript to pull the 96Boards blog posts - added the target='_blank' to the anchor tag

--- assets/js/app/resources.js
+++ assets/js/app/resources.js
@@ -1,23 +1,28 @@
-$(window).on("load", function () {
+function func(jsonData){
+    var listElements = '';
+    var len = jsonData.length;
+    for(var i=0;i<len;i++){
+        blogEntry = jsonData[i];
+        listElements += '<a target="_blank" href="' + blogEntry.url +'"><li class="list-group-item fly">' + blogEntry.title + '</li></a>';
+    }
+    console.log(listElements);
+    $("#boards-blogs").html(listElements);
+}
+
+$(document).ready(function () {
     if ($("#boards-blogs").length > 0) {
         // JSONP url
         var jsonp_url = "https://www.96boards.org/assets/json/blog.json?callback=func";
         // Add the JSONP to a script element
+        console.log("Adding script element");
         script = document.createElement("script");
         script.type = "text/javascript";
         script.src = jsonp_url;
-        
+        $("head").append(script);
         // This function handles the jsonp data we receive
         // Adds a list element for each result in the JSONP data
-        function func(jsonData){
-            var listElements = '';
-            var len = jsonData.length;
-            for(var i=0;i<len;i++){
-                blogEntry = jsonData[i];
-                listElements += '<a href="' + blogEntry.url +'"><li class="list-group-item fly">' + blogEntry.title + '</li></a>';
-            }
-            document.getElementById('boards-blog').innerHTML = listElements;
-        }
-
+    }
+    else{
+        console.log("Not defined!");
     }    
 });
