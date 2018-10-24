d20e0dde2f240dbdde0bec2f13f9ce853a342c24
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-10-24 16:41:32 +0100

    Updated the resources.js file to fetch the JSONP data from multiple sources, concat the data and then display the top 10 most recent posts

--- assets/js/app/resources.js
+++ assets/js/app/resources.js
@@ -1,26 +1,77 @@
+
+// Takes incoming JSON data and appends the data to allData array
+// This function handles the jsonp data we receive
+var allJSONData = []
+var counter = 0;
+// Define the sources to append the jsonp script elements and retreive the data.
+var sources = [
+    "https://www.96boards.org",
+    "https://www.trustedfirmware.org",
+    "https://www.op-tee.org"
+];
+
 function func(jsonData){
+    if(counter == (sources.length - 1)){
+        allJSONData = allJSONData.concat(jsonData);
+        addLatestNewsAndBlogs(allJSONData);
+    }
+    else{
+        allJSONData = allJSONData.concat(jsonData);
+        counter += 1;
+    }
+}
+// Process all JSON, get the latest news and blog posts and add to the list.
+function addLatestNewsAndBlogs(allJSONData){
+    console.log(data);
     var listElements = '';
-    var len = jsonData.length;
+    var len = allJSONData.length;
     for(var i=0;i<len;i++){
-        blogEntry = jsonData[i];
+        blogEntry = allJSONData[i];
         listElements += '<a target="_blank" href="' + blogEntry.url +'"><li class="list-group-item fly">' + blogEntry.title + '</li></a>';
     }
     console.log(listElements);
-    $("#boards-blogs").html(listElements);
+    $("#all-news-and-blogs").html(listElements);
 }
 
+// Check to see if the document has loaded 
 $(document).ready(function () {
-    if ($("#boards-blogs").length > 0) {
-        // JSONP url
-        var jsonp_url = "https://www.96boards.org/assets/json/blog.json?callback=func";
-        // Add the JSONP to a script element
-        console.log("Adding script element");
-        script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = jsonp_url;
-        $("head").append(script);
-        // This function handles the jsonp data we receive
-        // Adds a list element for each result in the JSONP data
+    // Check to see if the div we are adding to exists
+    if ($("#all-news-and-blogs").length > 0) {
+
+        // var allJSON = [];
+        // $.each(sources, function (index, url) {
+        //     var jsonp_url = url + "/assets/json/posts.json?callback=func";
+        //     var json_data = $.getJSON( jsonp_url, {
+        //         format: "jsonp"
+        //         })); // Request all data simultaneously
+        //     allJSON.push(json_data);
+        //     console.log(json_data);
+        //     console.log("Fetched the data for: " + jsonp_url);
+        // });
+        // $.when.apply($, allJSON).then(function () { // We have all data, merge it
+        //     var data = [];
+        //     $.each(allJSON, function (index, chunk) {
+        //         data = data.concat(chunk);
+        //     });
+        //     addLatestNewsAndBlogs(data); // Do whatever you want to this new collection of data
+        // });
+
+        // Loop through the sources and the separate script elements.
+        for(i=0;i<sources.length;i++){
+            // Adds a list element for each result in the JSONP data
+            // JSONP url
+            var jsonp_url = sources[i] + "/assets/json/posts.json?callback=func";
+            // Add the JSONP to a script element
+            console.log("Adding script element");
+            console.log(jsonp_url);
+            // Create a new script element and set the type and src
+            script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = jsonp_url;
+            // Append the new script element to the head.
+            $("head").append(script);
+        }
+
     }
     else{
         console.log("Not defined!");
