ca90276f941af29a9d22b94bdeb392d28ecc279b
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-10-25 10:14:38 +0100

    Added a separate js package for the blog search page

---
+++ assets/js/package-blog-search.js
@@ -0,0 +1,13 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/mc
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/owl.carousel
+//= require vendor/fuse
+
+
+//= require app/main
+//= require app/fly
+//= require app/resources
+//= require app/resources-blog-search
