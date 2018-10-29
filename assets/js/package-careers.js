d528bb04b978f651919efc384652b79d1a52d345
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-10-29 12:03:32 +0000

    Added the careers js package

---
+++ assets/js/package-careers.js
@@ -0,0 +1,12 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/mc
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+
+//= require app/main
+//= require app/fly
+//= require app/careers
+//= require app/recruiter-box-query-build.js
+
