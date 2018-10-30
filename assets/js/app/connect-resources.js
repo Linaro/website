57847887dcee057b1a349c8470965cb8a66a55da
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-10-30 10:01:08 +0000

    Added JS to fetch the connects.json file from connect.linaro.org and parse the latest connect json file and then show the top 10 results combined with the external resources json file

--- assets/js/app/connect-resources.js
+++ assets/js/app/connect-resources.js
@@ -1,35 +1,60 @@
 
 // This array stores the concatenated jsonp data
-var allJSONData = []
-// The counter variable counts the number of times results are added to the allJSONData array
+var allConnectJSONData = []
+// The counter variable counts the number of times results are added to the allConnectJSONData array
 // so we know when to process the concatenated data.
 var counter = 0;
 // Define the sources to append the jsonp script elements and retreive the data.
 var connect_sources = [
-    "https://staging.connect.linaro.org"
+    "https://connect.linaro.org"
 ];
 // Sort function which takes the data array, property to sort by and an asc boolean.
 function sort_by_date(a, b) {
     return new Date(b.date_published).getTime() - new Date(a.date_published).getTime();
 }
+
+// Get the connects.json file then parse and loop through each connect adding the jsonp script
+function connects(connectsJSON){
+    // Since we are just showing the top 10 resources just grab the first connect in the json output
+    var jsonp_url = connect_sources[0] + "/assets/json/" + connectsJSON[i].id.toLowerCase() + "/connects.json?callback=connects";
+    // Create a new script element and set the type and src
+    script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = jsonp_url;
+    // Append the new script element to the head.
+    $("head").append(script);
+}
 // This function handles the jsonp data we receive
 function connectResources(jsonData){
     if(counter == (connect_sources.length - 1)){
-        allJSONData = allJSONData.concat(jsonData);
-        var sorted_data = allJSONData.sort(sort_by_date);
-        addLatestNewsAndBlogs(sorted_data, 10);
+        allConnectJSONData = allConnectJSONData.concat(jsonData);
+        var sorted_data = allConnectJSONData.sort(sort_by_date);
+        addLatestResources(sorted_data, 10);
     }
     else{
-        allJSONData = allJSONData.concat(jsonData);
+        allConnectJSONData = allConnectJSONData.concat(jsonData);
         counter += 1;
     }
 }
+// This will fetch the external resources json from Linaro.org
+// function externalResources(jsonData){
+//     if(counter == (connect_sources.length - 1)){
+//         allConnectJSONData = allConnectJSONData.concat(jsonData);
+//         var sorted_data = allConnectJSONData.sort(sort_by_date);
+//         addLatestResources(sorted_data, 10);
+//     }
+//     else{
+//         allConnectJSONData = allConnectJSONData.concat(jsonData);
+//         counter += 1;
+//     }
+// }
+
 // Process all JSON, get the latest news and blog posts and add to the list.
-function addLatestNewsAndBlogs(allJSONData, number_of_items){
-    console.log(allJSONData);
+function addLatestResources(allConnectJSONData, number_of_items){
+    console.log(allConnectJSONData);
     var listElements = '';
     for(var i=0;i<number_of_items;i++){
-        post = allJSONData[i];
+        post = allConnectJSONData[i];
         listElements += '<a target="_self" href="' + post.url +'">';
         listElements += '<li class="list-group-item fly">';
         listElements += '<span class="post-title">' + post.title + '</span>';
@@ -40,6 +65,8 @@ function addLatestNewsAndBlogs(allJSONData, number_of_items){
     }
     $("#event-resources").html(listElements);
 }
+
+
 // Check to see if the document has loaded 
 $(document).ready(function () {
     // Check to see if the div we are adding to exists
@@ -48,7 +75,7 @@ $(document).ready(function () {
         for(i=0;i<connect_sources.length;i++){
             // Adds a list element for each result in the JSONP data
             // JSONP url
-            var jsonp_url = connect_sources[i] + "/assets/json/yvr18/data.json?callback=connectResources";
+            var jsonp_url = connect_sources[i] + "/assets/json/connects.json?callback=connects";
             // Add the JSONP to a script element
             // Create a new script element and set the type and src
             script = document.createElement("script");
