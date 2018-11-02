7692da85e9b01a29417a0051d25ee879d63d2797
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-11-02 16:26:49 +0000

    Modified resources/ js package and added the JS package for the event resources search

--- assets/js/package-event-resource-search.js
+++ assets/js/package-event-resource-search.js
@@ -4,11 +4,11 @@
 //= require vendor/cookieconsent
 //= require vendor/lazysizes
 //= require vendor/owl.carousel
+//= require vendor/underscore
+//= require vendor/fuzzy
+
 
 //= require app/main
 //= require app/fly
 //= require app/resources
-//= require app/connect-resources
-//= require app/project-stats
-
-
+//= require app/resources-event-search
