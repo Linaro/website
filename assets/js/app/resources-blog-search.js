e619317eeaba208aa4821567f37d1a1559fe11c9
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-11-13 11:43:29 +0000

    Added the ability to filter the results based on the checking of site specific checkboxes.

--- assets/js/app/resources-blog-search.js
+++ assets/js/app/resources-blog-search.js
@@ -5,7 +5,7 @@ var currentJSON = [];
 // The counter variable counts the number of times results are added to the allJSONData array
 // so we know when to process the concatenated data.
 var counter = 0;
-// Define the sources to append the jsonp script elements and retreive the data.
+// Blog/News JSON Sources
 var sources = [
     "https://www.96boards.org",
     "https://www.trustedfirmware.org",
@@ -48,6 +48,73 @@ function dynamicSort(property) {
             return a[property].localeCompare(b[property]);
         }        
     }
+}
+// Detects if an element is in an array
+function isInArray(value, array) {
+    return array.indexOf(value) > -1;
+}
+// Fuzzy Search Setup
+function filter_results_site(json_data, key, potential_keys) {
+    // Define the underscore.js template settings.
+    _.templateSettings = {
+        interpolate : /\{\{(.+?)\}\}/g
+    };  
+    // Specify a new html _.template
+    var listItemTemplate = _.template('<tr><td>{{post_title}}</td><td>{{post_author}}</td><td>{{post_date_published}}</td><td><a href="{{post_url}}">View post</a></td><td><a href="{{post_site}}"><img class="img-responsive" src="{{site_image}}"/></a></td></tr>');
+    // Get the search query val which we are searching for.
+    var search = $('#search-query').val();
+    // Fuzzy search options
+    var options = {
+        pre: "<b>"
+      , post: "</b>"
+
+      // Each element in the data is an object, not a string. We can pass in a
+      // function that is called on each element in the array to extract the
+      // string to fuzzy search against. In this case, element.dir
+      , extract: function(entry) {
+            return entry.title + '::' + entry.author;
+        }
+    }
+    // Filter!
+    var filtered = fuzzy.filter(search, json_data, options);
+    // Map the results to the html we want generated
+    var results = filtered.map(function(result){
+        var items = result.string.split('::');
+        // Check if the author is set and if not then replace with stripped url.
+        var author = result.original.author;
+        if(author == "undefined" || author == ""){
+            author = result.original.site.replace(/(^\w+:|^)\/\//, '');
+        }
+        else{
+            author = items[1];
+        }
+        var formatted_date = extractDateString(result.original.date_published);
+        var formatted_site = result.original.site.replace(/(^\w+:|^)\/\//, '');
+        var site_image = site_logos[result.original.site];
+        if (isInArray(result.original[key], potential_keys)){
+            return listItemTemplate({
+                post_url: result.original.url
+               , post_title: items[0]
+               , post_author: author
+               , post_date_published: formatted_date
+               , post_site: result.original.site
+               , post_site_formatted: formatted_site
+               , site_image: site_image
+               });
+        }
+    });
+    // Add original JSON array to currentJSON
+    var currentJSON = [];
+    
+    // Map the original item to a new currentJSON Array
+    filtered.map(function(item) {
+        if (isInArray(item.original[key], potential_keys)){
+            currentJSON.push(item.original);
+        }        
+    });
+    // Append results to the results html container
+    $('#result_size').html(filtered.length);
+    $('#results').html(results.join(''));
 }
 // Fuzzy Search Setup
 function listResults(json_data) {
@@ -129,15 +196,49 @@ function addFilters(allJSONData){
     console.log(authorElements);
     // Add unique sites to the site select
     var uniqueSites = getUniqueValuesOfKey(allJSONData, "site");
-    var  = result.original.site.replace(/(^\w+:|^)\/\//, '');
     var siteElements = '<div class="checkbox"><label><input type="checkbox" value="all-sites" id="all-sites" checked=checked>All Sites</label></div>';
     for(i=0;i<uniqueSites.length;i++){
-        var siteElements = siteElements + '<div class="checkbox"><label><input type="checkbox" value="'+ formatted_site +'" checked=checked id="' + formatted_site + '">'+ formatted_site +'</label></div>';
+        var formatted_site = uniqueSites[i].replace(/(^\w+:|^)\/\//, '');
+        var siteElements = siteElements + '<div class="checkbox"><label><input class="site-checkbox" type="checkbox" value="'+ uniqueSites[i] +'" id="' + formatted_site + '">'+ formatted_site +'</label></div>';
     }
     // Append the authors to the author select
     $("#site-checkboxes").append(siteElements);
     console.log(siteElements);
+
+    // Detect when a site url checkbox is checked and added any checked boxes to the checked_boxes array
+    $(".site-checkbox").click(function(){
+        // Set the all-sites checkbox to unchecked 
+        $("#all-sites").removeAttr("checked");
+        // Create a new checked_boxes array
+        var checked_boxes = [];
+        // Loop through all elements with the class site-checkbox and add
+        // those that are checked to the checked_boxes array
+        $(this).each(function(){
+            $(this).is(":checked", function(){
+                checked_boxes.push(($(this).attr("value")));
+            }); 
+        });
+        // Filter the results based on a key and an array of potential keys
+        filter_results(currentJSON, "site", checked_boxes);
+    });
+    // Detect when all sites checkbox is clicked and then toggle other checkboxes and list allJSONData
+    $("#all-sites").click(function(){
+        // If checkbox is already checked then make sure it stays checked. Toggling all-sites checkbox without selecting
+        // other site does nothing...
+        $(this).is(":checked", function(){
+            $(this).attr("checked");
+        }); 
+        $(".site-checkbox").each(function(){
+            // Set the all-sites checkbox to unchecked 
+            $(this).removeAttr("checked");
+        });
+        // Show all JSON data
+        listResults(allJSONData);
+    });
 }
+
+
+
 // This function handles the jsonp data we receive
 function func(jsonData){
     if(counter == (sources.length - 1)){
@@ -186,7 +287,6 @@ var delay = (function(){
       timer = setTimeout(callback, ms);
     };
   })();
-
 // This function sorts the data via a pre-defined filter
 function sortDataViaFilter(filter, toggle){
     // Filter data based on the date
@@ -205,22 +305,7 @@ function sortDataViaFilter(filter, toggle){
         }
     }
     // Filter the data based on the author
-    if(filter == "author"){
-        if(toggle == "desc"){
-            var sortedJsonData = currentJSON.sort(dynamicSort(filter));
-            listResults(sortedJsonData);
-            $("th.filter[data-filter='" + filter + "']").attr("data-toggle", "asc");
-        }
-        if(toggle == "asc"){
-            // Sort data by date desc using the currentJSON being displayed in the table
-            var sortedJsonData = currentJSON.sort(dynamicSort("-" + filter));
-            listResults(sortedJsonData);
-            // Set the new data-toggle value
-            $("th.filter[data-filter='" + filter + "']").attr("data-toggle", "desc");
-        }
-    }
-    // Filter the data based on the source
-    if(filter == "site"){
+    if(filter == "author" || filter == "site"){
         if(toggle == "desc"){
             var sortedJsonData = currentJSON.sort(dynamicSort(filter));
             listResults(sortedJsonData);
@@ -274,7 +359,6 @@ $(document).ready(function () {
                 else{
                     $(this).removeClass("active");
                 }
-                
             });
         }
     }
