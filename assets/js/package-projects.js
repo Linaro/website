a04134faca139f2ee9d52cd26e843d70fa04effb
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-23 13:15:38 +0000

    Added the Chart.min.js file and a new JS package for projects pages

---
+++ assets/js/package-projects.js
@@ -0,0 +1,9 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+//= require vendor/Chart.min
+
+//= require app/search
+//= require app/main
