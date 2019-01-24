915cd3e76a12ac48b1cf9a12e54fba89bb5de991
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-24 11:19:46 +0000

    Projects js ammendments

--- assets/js/app/projects.js
+++ assets/js/app/projects.js
@@ -1,30 +1,30 @@
+// Define the sources to prepend the jsonp script elements and retreive the data.
+// "" will use the current domain - this can be set to a cdn FQDN
+var patchesDataSources = [
+    ""
+];
+// This function handles the jsonp data we receive
+function patches(jsonData) {
+    //var sorted_data = sortByKeyAsc(jsonData, "title");
+    createPatchesGraphs(jsonData, 10);
+}
+// Process the perProjectPatches.json file and add data to relevant graphs
+function createPatchesGraphs(jsonData) {
+    var listElements = '';
+    for (var i = 0; i < jsonData[0].length; i++) {
+        console.log(jsonData[0][i]);
+        // Check to see whether the chart exists
+        // Grab the patches stats for each graph in the DOM
+        $(".projectStatsChart").each(function () {
+            var projectLinkName = $(this).data("link-name");
+            // var chartData = {};
+            console.log(projectLinkName);
+            // Create the data array for the charts 
+        });
+    }
+    $("#latest-patches").html(listElements);
+}
 $(window).on("load", function () {
-    // Define the sources to prepend the jsonp script elements and retreive the data.
-    // "" will use the current domain - this can be set to a cdn FQDN
-    var patchesDataSources = [
-        ""
-    ];
-    // This function handles the jsonp data we receive
-    function patches(jsonData) {
-        //var sorted_data = sortByKeyAsc(jsonData, "title");
-        createPatchesGraphs(jsonData, 10);
-    }
-    // Process the perProjectPatches.json file and add data to relevant graphs
-    function createPatchesGraphs(jsonData) {
-        var listElements = '';
-        for (var i = 0; i < jsonData[0].length; i++) {
-            console.log(jsonData[0][i]);
-            // Check to see whether the chart exists
-            // Grab the patches stats for each graph in the DOM
-            $(".projectStatsChart").each(function () {
-                var projectLinkName = $(this).data("link-name");
-                // var chartData = {};
-                console.log(projectLinkName);
-                // Create the data array for the charts 
-            });
-        }
-        $("#latest-patches").html(listElements);
-    }
     // Check to see if the patches graph canvas elements exist in the DOM
     if ($(".projectStatsChart").length > 0) {
         // Loop through the sources and the separate script elements.
