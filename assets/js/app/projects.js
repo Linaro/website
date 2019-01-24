406d184b87dd3d9c25ca7eeb44e0797fd0554e12
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-24 11:16:39 +0000

    JS to pull json data from the perProjectPatches.json file

--- assets/js/app/projects.js
+++ assets/js/app/projects.js
@@ -1,9 +1,47 @@
 $(window).on("load", function () {
-    // Check to see whether the chart exists
-    // Grab the patches stats for each graph in the DOM
-    $(".projectStatsChart").each(function () {
-        var projectLinkName = $(this).data("link-name");
-        console.log("Link name retreived!");
-        console.log(projectLinkName);
-    });
+    // Define the sources to prepend the jsonp script elements and retreive the data.
+    // "" will use the current domain - this can be set to a cdn FQDN
+    var patchesDataSources = [
+        ""
+    ];
+    // This function handles the jsonp data we receive
+    function patches(jsonData) {
+        //var sorted_data = sortByKeyAsc(jsonData, "title");
+        createPatchesGraphs(jsonData, 10);
+    }
+    // Process the perProjectPatches.json file and add data to relevant graphs
+    function createPatchesGraphs(jsonData) {
+        var listElements = '';
+        for (var i = 0; i < jsonData[0].length; i++) {
+            console.log(jsonData[0][i]);
+            // Check to see whether the chart exists
+            // Grab the patches stats for each graph in the DOM
+            $(".projectStatsChart").each(function () {
+                var projectLinkName = $(this).data("link-name");
+                // var chartData = {};
+                console.log(projectLinkName);
+                // Create the data array for the charts 
+            });
+        }
+        $("#latest-patches").html(listElements);
+    }
+    // Check to see if the patches graph canvas elements exist in the DOM
+    if ($(".projectStatsChart").length > 0) {
+        // Loop through the sources and the separate script elements.
+        for (i = 0; i < patchesDataSources.length; i++) {
+            // Adds a list element for each result in the JSONP data
+            // JSONP url
+            var jsonp_url = patchesDataSources[i] + "/assets/json/perProjectPatches.json?callback=patches";
+            // Add the JSONP to a script element
+            // Create a new script element and set the type and src
+            script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = jsonp_url;
+            // Append the new script element to the head.
+            $("head").append(script);
+        }
+    }
+    else {
+        console.log("No patches graphs found!");
+    }
 });
