4f350d0d1bcccb1fc1f1027f72471a3e85ab506d
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-03-12 10:58:43 +0000

    Updated the project-stats.js file with plain ajax request to the static.linaro.org resources (no longer using jsonp)

--- assets/js/app/project-stats.js
+++ assets/js/app/project-stats.js
@@ -1,12 +1,7 @@
 // Define the sources to append the jsonp script elements and retreive the data.
 var project_data_sources = [
-    ""
+    "https://static.linaro.org/assets/json/top10Projects.json"
 ];
-// This function handles the jsonp data we receive
-function projects(jsonData){
-    //var sorted_data = sortByKeyAsc(jsonData, "title");
-    addProjectsData(jsonData, 10);
-}
 // Process all JSON, get the latest news and blog posts and add to the list.
 function addProjectsData(sorted_data, number_of_items){
     var listElements = '';
@@ -22,22 +17,24 @@ function addProjectsData(sorted_data, number_of_items){
 // Check to see if the document has loaded 
 $(document).ready(function () {
     // Check to see if the div we are adding to exists
+    var jsonData = "";
     if ($("#latest-patches").length > 0) {
         // Loop through the sources and the separate script elements.
         for(i=0;i<project_data_sources.length;i++){
-            // Adds a list element for each result in the JSONP data
-            // JSONP url
-            var jsonp_url = project_data_sources[i] + "/assets/json/top10Projects.json?callback=projects";
-            // Add the JSONP to a script element
-            // Create a new script element and set the type and src
-            script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = jsonp_url;
-            // Append the new script element to the head.
-            $("head").append(script);
+            $.ajax({
+                url: project_data_sources[i],
+                dataType: 'json',
+                complete: function (jsonResponse) {
+                    jsonData = JSON.parse(jsonResponse.responseText);
+                }
+            });
         }
+        // Wait for all ajax requests to stop
+        $(document).ajaxStop(function (jsonData) {
+            addProjectsData(jsonData, 10);
+        });
     }
     else{
-        console.log("Not defined!");
+        console.log("Latest Patches JS included but #latest-patches is not defined!");
     }    
 });
