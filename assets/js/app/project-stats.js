2bf79e2559ab4b8ecccbacef27b699344626e9b5
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-03-13 10:22:04 +0000

    Updated the project-stats js to pull from the static.linaro.org s3 bucket

--- assets/js/app/project-stats.js
+++ assets/js/app/project-stats.js
@@ -2,6 +2,7 @@
 var project_data_sources = [
     "https://static.linaro.org/assets/json/top10Projects.json"
 ];
+var jsonData = "";
 // Process all JSON, get the latest news and blog posts and add to the list.
 function addProjectsData(sorted_data, number_of_items){
     var listElements = '';
@@ -17,7 +18,6 @@ function addProjectsData(sorted_data, number_of_items){
 // Check to see if the document has loaded 
 $(document).ready(function () {
     // Check to see if the div we are adding to exists
-    var jsonData = "";
     if ($("#latest-patches").length > 0) {
         // Loop through the sources and the separate script elements.
         for(i=0;i<project_data_sources.length;i++){
@@ -29,12 +29,14 @@ $(document).ready(function () {
                 }
             });
         }
-        // Wait for all ajax requests to stop
-        $(document).ajaxStop(function (jsonData) {
-            addProjectsData(jsonData, 10);
-        });
+
     }
     else{
         console.log("Latest Patches JS included but #latest-patches is not defined!");
     }    
+});
+// Wait for all ajax requests to stop
+$(document).ajaxStop(function () {
+    console.log(jsonData);
+    addProjectsData(jsonData, 10);
 });
