0933fde30371e6ec00cc39ec4d2672dda65d1773
Philip Colmer <philip.colmer@linaro.org>
2019-07-01 15:23:49 +0100

    Update build process

--- build-site.sh
+++ build-site.sh
@@ -7,12 +7,17 @@ fi
 if [ -z "$JEKYLL_ENV" ]; then
 	export JEKYLL_ENV=staging
 fi
-export JEKYLL_CONFIG="_config.yml,_config-$JEKYLL_ENV.yml"
-
-if [ ! -d "$(pwd)/.gems" ]; then
-        mkdir "$(pwd)/.gems"
-fi
-
-docker run --rm -it -p 4000:4000 -e JEKYLL_ACTION -e JEKYLL_CONFIG -e JEKYLL_ENV \
-        -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -v "$HOME":/srv/home \
-        -v "$(pwd)/.gems:/gems" -v "$(pwd)":/srv/source linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" build-site.sh
+docker run \
+  --cap-drop ALL \
+  --rm \
+  -it \
+  -p 4000:4000 \
+  -e JEKYLL_ACTION \
+  -e JEKYLL_CONFIG \
+  -e JEKYLL_ENV \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -u "$(id -u)":"$(id -g)" \
+  -v "$(pwd)":/srv/source \
+  linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" \
+  build-site.sh
