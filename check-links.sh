241d6b52f77e50cd2cff2627e7105284310ff419
Philip Colmer <philip.colmer@linaro.org>
2019-07-08 15:29:06 +0100

    Add script to do link checking

---
+++ check-links.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Calls the link checker container to run the tool against the current directory.
+#
+# The script is slightly more complicated that it could otherwise be because the
+# parameters to the tool can reference files and these will typically be in the
+# directory above the current directory, so we have to volume-map the parent
+# directory and tell the tool to check *this* directory.
+
+DIRNAME=$(basename `pwd`)
+PARENT=$(dirname $(readlink -f .))
+
+if [ -z "$LINKCHECK" ]; then
+  LINKCHECK=latest
+fi
+
+docker run \
+  --cap-drop ALL \
+  --rm \
+  -t \
+  -v $PARENT:/srv
+  linaroits/linkcheck:"$LINKCHECK" \
+  -d "$DIRNAME" "$@"
