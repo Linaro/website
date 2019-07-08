fec574713b476f58c9b3d1fcb99e71e031b5d091
Philip Colmer <philip.colmer@linaro.org>
2019-07-08 16:40:55 +0100

    Check where script is being run

--- check-links.sh
+++ check-links.sh
@@ -7,11 +7,21 @@
 # directory above the current directory, so we have to volume-map the parent
 # directory and tell the tool to check *this* directory.
 
-DIRNAME=$(basename `pwd`)
-PARENT=$(dirname $(readlink -f .))
+DIRNAME="$(basename `pwd`)"
+PARENT="$(dirname $(readlink -f .))"
 
 if [ -z "$LINKCHECK" ]; then
   LINKCHECK=latest
+fi
+
+# Make sure that the script is not in the current directory. The output from Jekyll
+# should be in a sub-directory of the git repository.
+SCRIPTDIR="$(readline -f "$0")"
+if [ "$SCRIPTDIR" == "$(pwd)" ]; then
+  echo "Script is being run incorrectly. Go into the built site directory and"
+  echo "then run:"
+  echo "../build-site.sh"
+  exit 1
 fi
 
 docker run \
