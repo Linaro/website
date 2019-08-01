Linaro/website

commit f18d599a6d5abc19816295dfa29846026472719b

Added Li Gong video to about page - updated homepage slider (#727)
* Updating legal page (#720)

* Added a redirect for the membership rules pdf.

* Added the Antitrust Guidelines tab to the legal page

* Added the IPR policy to the legal page

* Added the antitrust-policy redirect

* Added the IPR policy redirect to the IPR policy tab under the legal/ page

* Removing Horizon Robotics/PCL PR slides (#726)

* Removed the Horizon robotics / PCL PR's from the slider

* Fixes for broken links to source files

* Fixing additional broken link

* Adding Introduction to Linaro video (#723)

* Bumped theme version

* Added the Introduction to Linaro video by Li Gong

* Removed the foundations sectiopn

* Removed Francois/Jarmo from the TSC page (#725)

Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 # Jekyll Theme
-gem 'jumbo-jekyll-theme', "5.5.1"
+gem 'jumbo-jekyll-theme', "5.5.4"
 # Jekyll Plugins
 group :jekyll_plugins do
    gem "jekyll-data"

_posts/2011/02/2011-02-15-snowball-new-cortex-a9-community-board-from-st-ericsson.markdown
@@ -88,4 +88,4 @@ A new website has been created to host the community for this new platform: [htt
 
 At Linaro, we welcome this new member of the Cortex A9 chip family, and will support this board in the upcoming releases that we produce every 6 months. Perhaps not in the 11.05 release which is only 3 months away, but most probably in the 11.11 one. The ST-Ericsson landing team will take care of mainlining its Linux BSP.
 
-The board will be sold on the [http://www.calao-store.com/](http://www.calao-store.com/) website. All technical details will be found on the CALAO Systems website, and on the Igloo Community website. By the way, did you notice that Linux is no longer mentioned in recent press releases? Look at [ST-Ericsson's announcement](http://www.stericsson.com/press_releases/Igloo_Snowball.jsp) for example. I conclude that the operating system choice is getting obvious now...
+The board will be sold on the http://www.calao-store.com/ website. All technical details will be found on the CALAO Systems website, and on the Igloo Community website. By the way, did you notice that Linux is no longer mentioned in recent press releases? Look at [ST-Ericsson's announcement](http://www.stericsson.com/press_releases/Igloo_Snowball.jsp) for example. I conclude that the operating system choice is getting obvious now...

_posts/2016/06/2016-06-27-coresight-perf-and-the-opencsd-library.markdown
@@ -42,11 +42,11 @@ All examples presented in this post have been collected on a juno-R0 platform us
 ### **Background on Perf and the Performance Management Units**
 
 
-The standard Perf core is a performance analysis tool found in the Linux kernel. It comes with a complement user space tool, simply called _perf_, that provides a suite of sub-commands to control and present trace profiling sessions. Perf is most commonly used to access SoC performance counters, but over the years it has grown well beyond that and now covers tracepoints, software performance counters and dynamic probes. 
+The standard Perf core is a performance analysis tool found in the Linux kernel. It comes with a complement user space tool, simply called _perf_, that provides a suite of sub-commands to control and present trace profiling sessions. Perf is most commonly used to access SoC performance counters, but over the years it has grown well beyond that and now covers tracepoints, software performance counters and dynamic probes.
 
-The perf core is generic and caters to many architectures. To hide variations between HW implementation and profiling metrics the concept of Performance Monitoring Unit (PMU) is used. A PMU is a [structure](http://lxr.free-electrons.com/source/include/linux/perf_event.h?v=4.6#L223) providing a well defined set of interfaces that PMU drivers implement in order to carry action on behalf of the Perf core. The actions carried out the by the PMU drivers are not relevant to the Perf core itself, as long as the semantic of the API is respected.
+The perf core is generic and caters to many architectures. To hide variations between HW implementation and profiling metrics the concept of Performance Monitoring Unit (PMU) is used. A PMU is a [structure](https://elixir.bootlin.com/linux/latest/source/include/linux/perf_event.h?v=4.6#L223) providing a well defined set of interfaces that PMU drivers implement in order to carry action on behalf of the Perf core. The actions carried out the by the PMU drivers are not relevant to the Perf core itself, as long as the semantic of the API is respected.
 
-Every time a process is installed on a CPU for execution, the scheduler invokes the Perf core.  From there Perf will see if any event is associated with that process and if so, the PMU API performing HW specific operations is invoked. The same happens when the process is removed from a CPU. That way statistics and performance counters are collected for that process only and aren’t impacted by other activities concurrently happening in the system. Traces collected during a session are transferred to user space using a mmap’ed area and made available to users in the _perf.data_ file. The latter is then read by the various _perf_ [sub-command](https://perf.wiki.kernel.org/index.php/Main_Page) for rendering in human readable format. 
+Every time a process is installed on a CPU for execution, the scheduler invokes the Perf core.  From there Perf will see if any event is associated with that process and if so, the PMU API performing HW specific operations is invoked. The same happens when the process is removed from a CPU. That way statistics and performance counters are collected for that process only and aren’t impacted by other activities concurrently happening in the system. Traces collected during a session are transferred to user space using a mmap’ed area and made available to users in the _perf.data_ file. The latter is then read by the various _perf_ [sub-command](https://perf.wiki.kernel.org/index.php/Main_Page) for rendering in human readable format.
 
 Integrating the CoreSight drivers with the Perf core was advantageous on many fronts. On the kernel side it streamlined the configuration of trace sessions - with hundreds of parameters per CPU this was certainly not something to pass on. It also offered a way to easily transfer massive amounts of trace data to user space with little overhead. In user space the metadata pertaining to each trace session could be embedded in the _perf.data_ file and _perf_ sub-commands like _report_ and _script_ used to decode trace data. Last but not least most of the upstream code can be re-used in the PMU abstraction.
 
@@ -133,7 +133,7 @@ In user space integration is done around three tools: _perf record_, _perf repor
 ##### **perf record**
 
 
-Integration in the _perf record_ sub-command is done by providing an architecture specific function that return a [struct auxtrace_record](http://lxr.free-electrons.com/source/tools/perf/util/auxtrace.h?v=4.6#L292).  As with the kernel PMU abstraction the auxtrace_record structure allows the generic core to perform architecture-specific operations without losing genericity.  That way it is possible to process traces data generated by IntePT and CoreSight without changing anything to the common core.
+Integration in the _perf record_ sub-command is done by providing an architecture specific function that return a [struct auxtrace_record](https://elixir.bootlin.com/linux/latest/source/tools/perf/util/auxtrace.h?v=4.6#L292).  As with the kernel PMU abstraction the auxtrace_record structure allows the generic core to perform architecture-specific operations without losing genericity.  That way it is possible to process traces data generated by IntePT and CoreSight without changing anything to the common core.
 
 ```c
 struct auxtrace_record *cs_etm_record_init(int *err)
@@ -247,7 +247,7 @@ _mpoirier@t430:~/work/linaro/coresight/jun01-user$ perf report --stdio_
 
 The above shows that 4.13% of all the instruction ranges started in library libc-2.21.so at address 0x0000000000078758.  Using the source code, the DSO file and an objdump utility it is possible to quickly identify the function that was referenced.  It is important to keep in mind that flame graphs are generated using the _entry_ point only.  Nothing can be deduced about the path through the code that was taken after that.
 
-From more accurate results it is suggested to work with the _script_ command where a user supplied script can take advantage of all the information conveyed by synthesised events by way of the [perf_sample structure](http://lxr.free-electrons.com/source/tools/perf/util/event.h#L180).  An example is the _cs-trace-disasm.py_ script produced by Linaro:
+From more accurate results it is suggested to work with the _script_ command where a user supplied script can take advantage of all the information conveyed by synthesised events by way of the [perf_sample structure](https://elixir.bootlin.com/linux/latest/source/tools/perf/util/event.h#L180).  An example is the _cs-trace-disasm.py_ script produced by Linaro:
 
 
 ```c

_posts/2016/09/2016-09-06-stm-and-its-usage.markdown
@@ -524,11 +524,11 @@ The above trace data was just decoded from the sample program [6], the red numbe
 
 I hope this post provided an useful introduction to STM and presented how to use it clearly.  One work related to STM is still ongoing,  which I have been doing - Ftrace integration with STM, function traces can be exported via STM once this work is done.  At this moment the 5th iteration [8] for this feature has been released.
 
-[1] [http://lxr.free-electrons.com/source/drivers/hwtracing/stm/console.c?v=4.7](http://lxr.free-electrons.com/source/drivers/hwtracing/stm/console.c?v=4.7)
+[1] [https://elixir.bootlin.com/linux/latest/source/drivers/hwtracing/stm/console.c?v=4.7](https://elixir.bootlin.com/linux/latest/source/drivers/hwtracing/stm/console.c?v=4.7)
 
-[2] [http://lxr.free-electrons.com/source/arch/arm64/boot/dts/sprd/sc9836.dtsi?v=4.3#L178](http://lxr.free-electrons.com/source/arch/arm64/boot/dts/sprd/sc9836.dtsi?v=4.3#L178)
+[2] [https://elixir.bootlin.com/linux/latest/source/arch/arm64/boot/dts/sprd/sc9836.dtsi?v=4.3#L178](https://elixir.bootlin.com/linux/latest/source/arch/arm64/boot/dts/sprd/sc9836.dtsi?v=4.3#L178)
 
-[3] [http://lxr.free-electrons.com/source/drivers/hwtracing/coresight/coresight-stm.c](http://lxr.free-electrons.com/source/drivers/hwtracing/coresight/coresight-stm.c)
+[3] [https://elixir.bootlin.com/linux/latest/source/drivers/hwtracing/coresight/coresight-stm.c](https://elixir.bootlin.com/linux/latest/source/drivers/hwtracing/coresight/coresight-stm.c)
 
 [4] [https://patchwork.kernel.org/patch/9189197/](https://patchwork.kernel.org/patch/9189197/)
 

commit f0ce9d845623b7457e751b371caedd7289a884a5

Pulling in changes from develop (#734)
* Updating legal page (#720)

* Added a redirect for the membership rules pdf.

* Added the Antitrust Guidelines tab to the legal page

* Added the IPR policy to the legal page

* Added the antitrust-policy redirect

* Added the IPR policy redirect to the IPR policy tab under the legal/ page

* Removing Horizon Robotics/PCL PR slides (#726)

* Removed the Horizon robotics / PCL PR's from the slider

* Fixes for broken links to source files

* Fixing additional broken link

* Adding Introduction to Linaro video (#723)

* Bumped theme version

* Added the Introduction to Linaro video by Li Gong

* Removed the foundations sectiopn

* Removed Francois/Jarmo from the TSC page (#725)

* Fix Bio typo (#728)

Broken in PR #727 
[https://github.com/Linaro/website/pull/727/commits/0354f16be9f57844b3c007bfd6038a624fa1eab4]

* Fix `serve` action (#730)

Typo fix

* Fixing jekyll doctor issues (#733)

* Fixing conflicting names of posts

* Removing unwanted blog post

* Removing duplicate post

* Removing duplicate empty posts

* Deleted duplicate post

* Fixing broken link

* Updated the theme version (#732)

Gemfile
@@ -1,11 +1,10 @@
 source "https://rubygems.org"
 # Jekyll Theme
-gem 'jumbo-jekyll-theme', "5.5.4"
+gem 'jumbo-jekyll-theme', "5.5.5"
 # Jekyll Plugins
 group :jekyll_plugins do
    gem "jekyll-data"
    gem "nokogiri"
-   gem 'jekyll', '~> 3.7.4'
 end
 # # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 # gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]

_posts/2016/09/2016-09-02-hisilicon-creates-poplar-the-first-96boards-tv-platform-development-board.markdown
@@ -43,9 +43,8 @@ Please visit LHG at[ LAS16](https://connect.linaro.org/las16/) to see some of th
 
 The 96Boards Poplar Hardware User Manual is available here:[https://www.96boards.org/documentation/enterprise/poplar/hardware-docs/hw-user-manual.md.html](https://www.96boards.org/documentation/enterprise/poplar/hardware-docs/hw-user-manual.md.html)
 
-The 96Boards Poplar board is currently on sale for $79USD + Shipping on Aliexpress ([http://www.aliexpress.com/store/product/96Boards-Poplar/2350001_32701867575.html?spm=2114.8147860.0.85.jZ7Tzj](http://www.aliexpress.com/store/product/96Boards-Poplar/2350001_32701867575.html?spm=2114.8147860.0.85.jZ7Tzj)).
+The 96Boards Poplar board is currently on sale for $79USD + Shipping on Aliexpress ([https://www.aliexpress.com/item/32811234610.html?spm=2114.12010615.0.0.UxfuRI](https://www.aliexpress.com/item/32811234610.html?spm=2114.12010615.0.0.UxfuRI)).
 
 Linaro has officially released the 96Boards TV Platform Specification at this location:[ http://www.96boards.org/specifications/](http://www.96boards.org/specifications/).
 
 The Poplar board is supported on the 96Boards website at:[ http://www.96boards.org/product/poplar/](http://www.96boards.org/product/poplar/)
-

_posts/2017/01/2017-01-31-las16.markdown
@@ -1,28 +1,0 @@
----
-amazon_s3_presentation_url: None
-amazon_s3_video_url: None
-author: connect
-categories:
-- blog
-comments: false
-date: 2017-01-31 14:39:28
-excerpt: LAS16 Playlist
-image:
-  featured: true
-  path: /assets/images/blog/LAS16Playlist.jpg
-layout: post
-link: http://connect.linaro.org/playlist/las16/
-session_id: None
-session_track: None
-slideshare_presentation_url: None
-slug: las16
-speakers: None
-title: LAS16 Playlist
-video_length: 01:40
-video_thumbnail: None
-wordpress_id: 4516
-youtube_video_url: None
-permalink: /blog/:title/
----
-
-{% include media.html media_url="https://www.youtube.com/embed/videoseries?list=PLKZSArYQptsPD8fQJvlL6fegGCplRF8jh" %}

_posts/op-tee/2017-03-17-bof-device-tree-secure-firmware-bud17-313.markdown
@@ -1,24 +1,0 @@
----
-author: op-tee
-tags: 
-- OP-TEE
-title: 'Bud17-313 BoF &#58; Device Tree and Secure Firmware'
-date: 2017-03-17 12:00:00 Z
-categories:
-- Blog
-layout: post
-image:
-    featured: true
-    path: /assets/images/blog/bof-device-tree-secure-firmware-bud17-313-image.jpg
----
-
-{% include media.html media_url="https://www.youtube.com/embed/kbREjQS3moM" %}
-
-Device Tree is well established in the Linux kernel. But since there could be other bootloader(s) and firmware components involved that needs to configure the hardware and thereby also needs to update the Device Tree blobs before passing it to Linux kernel. Therefore we are looking for a well established way for firmware to also make use and modify the Device Tree blobs before handing them over to Linux kernel. With this BoF session we would like to get started a gather ideas etc
-
-{% include media.html media_url="//www.slideshare.net/slideshow/embed_code/key/w10DZhBzHkyykA" %}
-
-[BUD17-416: Benchmark and profiling in OP-TEE ](https://www.slideshare.net/linaroorg/bud17416-benchmark-and-profiling-in-optee) from [Linaro](http://www.slideshare.net/linaroorg)  
-**Speakers:** Joakim Bech, Jens Wiklander  
-**Track:** Security  
-**Session ID:** BUD17-313  