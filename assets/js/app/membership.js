df816a526ec686f61b598486b01a33409bfd66e4
Kyle <kyle.kirkby@linaro.org>
2019-08-23 10:48:22 +0100

    Website Content Updates (#751)

--- assets/js/app/membership.js
+++ assets/js/app/membership.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip({ container: 'body'})
     });
-    
+
     var projectSlider = $("#projects-slider");
-    
+
     projectSlider.owlCarousel({
         items: 4,
         loop: true,
@@ -32,9 +32,9 @@ $(document).ready(function () {
             }
         }
     });
-    
+
     var relatedNewsSlider = $("#related-news-slider");
-    
+
     relatedNewsSlider.owlCarousel({
         items: 4,
         loop: true,
@@ -59,5 +59,28 @@ $(document).ready(function () {
             }
         }
     });
-});
 
+    var testimonialSlider = $("#testimonial_slider");
+
+    testimonialSlider.owlCarousel({
+        items: 4,
+        loop: false,
+        dots: false,
+        nav: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 8000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+});
