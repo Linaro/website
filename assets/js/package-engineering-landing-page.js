3b7a3d4ac09898ed19f964033871a5b01e6114e9
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-11-01 15:48:14 +0000

    Added an engineering-landing-page JS package which adds the JS to pull down related resources

---
+++ assets/js/package-engineering-landing-page.js
@@ -0,0 +1,8 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+
+//= require app/search
+//= require app/main
+//= require app/engineering-landing-page
