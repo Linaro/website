b63337b21655389a0e28b4b41c1262c01ab18e29
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-12-03 15:57:13 +0000

    Homepage js updates

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,33 +1,6 @@
 // When the DOM is ready
-$(function () {
-    $("#engineering_homepage_slider").owlCarousel({
-      loop: false,
-      navigation: false,
-      nav: false,
-      dots: true,
-      lazyLoad: true,
-      margin: 25,
-      autoplay: false,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 2,
-          nav: true
-        },
-        733: {
-          items: 3,
-          nav: true
-        },
-        1082: {
-          items: 4,
-          nav: true
-        },
-        1200: {
-          items: 4,
-          nav: true
-        }
-      }
-    });
+$(document).ready(function () {
+
     $("#engineering_homepage_slider_mobile").owlCarousel({
         loop: false,
         nav: false,
@@ -37,6 +10,16 @@ $(function () {
         autoplay: false,
         items: 1
     });
+    $("#engineering_homepage_slider").owlCarousel({
+        loop: false,
+        nav: false,
+        dots: true,
+        lazyLoad: true,
+        margin: 25,
+        autoplay: false,
+        items: 4
+    });
+
     $("#testimonial_slider").owlCarousel({
         loop: false,
         nav: false,
