a4c3d1ae06639983b8909a7b49398cc5da6c5e67
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-12-05 12:09:47 +0000

    Removed uncessary js files in bundle

--- assets/js/package-about.js
+++ assets/js/package-about.js
@@ -4,12 +4,7 @@
 //= require vendor/owl.carousel
 //= require vendor/lazysizes
 //= require vendor/ls.unveilhooks
-//= require app/sticky-tab-bar
-//= require vendor/featherlight
 
-//= require app/search
 //= require app/main
 //= require app/about
 //= require app/search
-//= require app/fly
-//= require app/tables
