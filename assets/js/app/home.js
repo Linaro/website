192bcee7ed507a3bfc4565fb14030815b72c09b3
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-12-11 15:18:52 +0000

    Removed the video modal js since this has been added to a separate file

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,50 +1,40 @@
 // When the DOM is ready
-$(document).ready(function () {
-    $(".video_modal").on("shown.bs.modal", function(e) {
-        var videoElement = $(this).find("video");
-        videoElement.trigger('play');
-    });
-    $(".video_modal").on("hide.bs.modal", function(e) {
-        var videoElement = $(this).find("video");
-        videoElement.trigger("pause");
-    });
+$(document).ready(function() {
+  $("#engineering_homepage_slider_mobile").owlCarousel({
+    loop: false,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    margin: 25,
+    autoplay: true,
+    rewind: true,
+    autoHeight: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    items: 1
+  });
 
-    $("#engineering_homepage_slider_mobile").owlCarousel({
-        loop: false,
-        nav: false,
-        dots: true,
-        lazyLoad: true,
-        margin: 25,
-        autoplay: true,
-        rewind: true,
-        autoHeight: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        items: 1
-    });
+  $("#engineering_homepage_slider").owlCarousel({
+    loop: false,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    margin: 25,
+    autoHeight: true,
+    autoplay: true,
+    rewind: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 4000,
+    items: 4
+  });
 
-    $("#engineering_homepage_slider").owlCarousel({
-        loop: false,
-        nav: false,
-        dots: true,
-        lazyLoad: true,
-        margin: 25,
-        autoHeight: true,
-        autoplay: true,
-        rewind: true,
-        autoplayHoverPause: true,
-        autoplayTimeout: 4000,
-        items: 4
-    });
-
-    $("#testimonial_slider").owlCarousel({
-        loop: false,
-        nav: false,
-        dots: true,
-        lazyLoad: true,
-        margin: 25,
-        autoplay: false,
-        items: 1
-    });
-
+  $("#testimonial_slider").owlCarousel({
+    loop: false,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    margin: 25,
+    autoplay: false,
+    items: 1
+  });
 });
