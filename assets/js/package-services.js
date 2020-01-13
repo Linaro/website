7d2e475fe66f542af0ea779da8fee47f6371c9d2
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-01-13 14:15:46 +0000

    Updated the services js

---
+++ assets/js/package-services.js
@@ -0,0 +1,10 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+//= require vendor/owl.carousel
+
+//= require app/search
+//= require app/main
+//= require app/components/developer_services_form
