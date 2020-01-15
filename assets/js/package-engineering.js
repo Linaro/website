6f2d961b38d75477429a9e58351e00c68f246037
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-01-15 08:59:53 +0000

    Added the owl carousel library

--- assets/js/package-engineering.js
+++ assets/js/package-engineering.js
@@ -2,8 +2,11 @@
 //= require vendor/bootstrap
 //= require vendor/cookieconsent
 //= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+//= require vendor/owl.carousel
 
 //= require app/search
 //= require app/main
 //= require app/engineering
 //= require app/components/engineering_slider
+#
