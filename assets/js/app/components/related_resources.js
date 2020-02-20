b335113f7b77e440925d2fedbcda5d3bc303f8bf
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-20 13:39:28 +0000

    Further cross browser flexibility. Constructing a new Date object from the year, month and day values

--- assets/js/app/components/related_resources.js
+++ assets/js/app/components/related_resources.js
@@ -72,8 +72,12 @@ $(document).ready(function() {
       data
     ) {
       $.each(data, function(key, val) {
-        var date_string = val["start_date"].split(" ")[0];
-        var start_date = new Date(date_string);
+        var split_date_string = val["start_date"].split(" ")[0].split("-");
+        var start_date = new Date(
+          split_date_string[0],
+          split_date_string[1],
+          split_date_string[2]
+        );
         if (start_date.getTime() < new Date().getTime()) {
           // Get the JSON url for each Linaro Connect
           var json_url =
