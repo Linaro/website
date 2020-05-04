7ea4b989c2a3d58f9c5d32b0481d126f1f8f0070
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-05-04 14:18:08 +0100

    Updates to the projects slider

--- assets/js/app/custom.js
+++ assets/js/app/custom.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+  if ($("#projects_slider").length > 0) {
+    $("#projects_slider").owlCarousel({
+      items: 4,
+      loop: true,
+      dots: false,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 2500,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 2,
+        },
+        400: {
+          items: 4,
+        },
+        700: {
+          items: 6,
+        },
+        1000: {
+          items: 8,
+        },
+        1200: {
+          items: 9,
+        },
+      },
+    });
+  }
   if ($("#members_slider").length > 0) {
     $("#members_slider").owlCarousel({
       loop: false,
@@ -61,35 +89,6 @@ $(document).ready(function () {
       autoplayHoverPause: true,
       autoplayTimeout: 4000,
       items: 4,
-    });
-  }
-  if ($("#projects_slider").length > 0) {
-    var projectSlider = $("#projects_slider");
-    projectSlider.owlCarousel({
-      items: 4,
-      loop: true,
-      dots: false,
-      margin: 10,
-      autoplay: true,
-      autoplayTimeout: 2500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 2,
-        },
-        400: {
-          items: 4,
-        },
-        700: {
-          items: 6,
-        },
-        1000: {
-          items: 8,
-        },
-        1200: {
-          items: 9,
-        },
-      },
     });
   }
 });
