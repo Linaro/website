e6735165dc8c46e9f2065cc64053fc37b15f568e
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-05-29 14:26:10 +0100

    Correcting url updates

--- remote/js/linarofamily.js
+++ remote/js/linarofamily.js
@@ -1,3 +1,3 @@
 document.write(
-  '<div id="family">Looking for <a href="http://www.linaro.org/">Linaro.org</a><a href="http://www.linaro.org/careers/">Careers</a><a href="http://www.linaro.org/linaro-blog/">Blog</a><a href="http://wiki-archive.linaro.org/">Wiki</a><a href="https://connect.linaro.org/">Linaro Connect</a><a href="http://ask.linaro.org/">Ask Linaro</a><a href="https://dash.linaro.org/">Project status</a><a href="https://www.linaro.org/planet/">Planet Linaro</a></div>'
+  '<div id="family">Looking for <a href="http://www.linaro.org/">Linaro.org</a><a href="http://www.linaro.org/careers/">Careers</a><a href="http://www.linaro.org/linaro-blog/">Blog</a><a href="http://wiki-archive.linaro.org/">Wiki</a><a href="https://connect.linaro.org/">Linaro Connect</a><a href="http://ask.linaro.org/">Ask Linaro</a><a href="https://www.linaro.org/planet/">Planet Linaro</a></div>'
 );
