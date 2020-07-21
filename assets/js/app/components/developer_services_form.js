be8154534b32b3366a613b933119b9cf8c65d0ce
Philip Colmer <philip.colmer@linaro.org>
2020-07-21 11:24:21 +0100

    Try another way ...

--- assets/js/app/components/developer_services_form.js
+++ assets/js/app/components/developer_services_form.js
@@ -1,30 +1,41 @@
-$(document).ready(function() {
-  console.log("Document ready function firing ...");
-  $("#btnSubmit").click(function(event) {
-    console.log("Feedback form submit firing ...");
-    event.preventDefault();
-    var form = $('#feedback-form')[0];
-    var data = new FormData(form);
-    console.log(data);
-    var jqxhr = $.ajax({
-      type: "POST",
-      enctype: 'multipart/form-data',
-      url: 'https://servicedesk.linaro.org/plugins/servlet/feedback/create',
-      data: data,
-      processData: false,
-      contentType: false,
-      cache: false,
-    })
-      .done(function(data) {
-        $('#feedback-form').hide();
-        $('#feedback-response').html(data);
-      })
-      .fail(function(e) {
-        console.log("ERROR: ", e);
-      })
+// $(document).ready(function() {
+//   console.log("Document ready function firing ...");
+//   $("#btnSubmit").click(function(event) {
+//     console.log("Feedback form submit firing ...");
+//     event.preventDefault();
+//     var form = $('#feedback-form')[0];
+//     var data = new FormData(form);
+//     console.log(data);
+//     var jqxhr = $.ajax({
+//       type: "POST",
+//       enctype: 'multipart/form-data',
+//       url: 'https://servicedesk.linaro.org/plugins/servlet/feedback/create',
+//       data: data,
+//       processData: false,
+//       contentType: false,
+//       cache: false,
+//     })
+//       .done(function(data) {
+//         $('#feedback-form').hide();
+//         $('#feedback-response').html(data);
+//       })
+//       .fail(function(e) {
+//         console.log("ERROR: ", e);
+//       })
 
-    jqxhr.always(function() {
-      console.log("Post finished");
-    })
-  });
-});
+//     jqxhr.always(function() {
+//       console.log("Post finished");
+//     })
+//   });
+// });
+
+feedback_form.onsubmit = async (e) => {
+  e.preventDefault();
+  let response = await fetch(
+    'https://servicedesk.linaro.org/plugins/servlet/feedback/create', {
+      method: 'POST',
+      body: new FormData(feedback_form)
+    });
+  let result = await response.text();
+  console.log(result);
+}
