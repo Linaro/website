2f0573dd07246114436a63151085f2f3a1d1c9fe
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-12-02 11:29:04 +0000

    Added JS for custom video embed

--- assets/js/app/about.js
+++ assets/js/app/about.js
@@ -1,4 +1,16 @@
 $(document).ready(() => {
+  // YouTube embed
+  const videoEmbed = $(".videoPlayer");
+  function updateVideo() {
+    const id = $(".videoPlayer").attr("id").replace("vid-", "");
+    $(".videoPlayer").addClass("embed-responsive-16by9"); // adds responsive styling to the video iframe via a css class
+    $(".videoPlayer").html(
+      '<iframe src="//www.youtube.com/embed/' +
+        id +
+        '?autoplay=1" width="560" height="315" frameborder="0" allowfullscreen></iframe>'
+    );
+  }
+  videoEmbed.on("click", updateVideo);
   // Fetch the maintainers data
   const maintainersByProjectData = JSON.parse(
     decodeURIComponent(
@@ -55,6 +67,7 @@ $(document).ready(() => {
     },
     options: {
       cutoutPercentage: 30,
+      maintainAspectRatio: false,
       responsive: true,
       legend: {
         position: "bottom",
@@ -80,6 +93,7 @@ $(document).ready(() => {
     },
     options: {
       responsive: true,
+      maintainAspectRatio: false,
       cutoutPercentage: 30,
       legend: {
         position: "bottom",
