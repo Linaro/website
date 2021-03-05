275e3b018ef4ac7f4baf674d7603a5a5e251983c
Kyle Kirkby <kyle.kirkby@linaro.org>
2021-03-05 15:17:42 +0000

    Switching API endpoint to production

--- assets/js/app/components/theme_contact_form.js
+++ assets/js/app/components/theme_contact_form.js
@@ -42,11 +42,11 @@ theme_contact_form.onsubmit = (e) => {
     formDataPayloadBody[checkboxGroupName] = data.getAll(checkboxGroupName);
     // Send the POST request.
     fetch(
-      "https://pvwhresjz0.execute-api.us-east-1.amazonaws.com/dev/formSubmit",
+      "https://avqfk3gzg2.execute-api.us-east-1.amazonaws.com/prod/formSubmit",
       {
         method: "POST",
         body: JSON.stringify(formDataPayloadBody),
-        headers: { "X-Api-Key": "xpIcQXHk0Y1XUmBBH4xUl5ScFxgddlFnEpQeSzO3" },
+        headers: { "X-Api-Key": "ox9fSkYfRK16mxy5Gv6pM121H7iAubAQ6uzsDmoW" },
       }
     )
       .then((response) => response.json())
